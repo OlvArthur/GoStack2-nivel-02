064625250c98e999ca1ef61e42a63085841caa15
Arthur Oliveira <olvarthur@gmail.com>
2020-04-18 03:21:07 -0300

    Uploading files with multer

--- iniciando-back-end-app/aulas/src/database/migrations/1587142967487-CreateUsers.ts
+++ iniciando-back-end-app/aulas/src/database/migrations/1587142967487-CreateUsers.ts
@@ -27,6 +27,11 @@ export default class CreateUsers1587142967487 implements MigrationInterface {
             type: 'varchar',
           },
           {
+            name: 'avatar',
+            type: 'varchar',
+            isNullable: true,
+          },
+          {
             name: 'created_at',
             type: 'timestamp',
             default: 'now()',
--- iniciando-back-end-app/aulas/src/routes/users.routes.ts
+++ iniciando-back-end-app/aulas/src/routes/users.routes.ts
@@ -1,18 +1,24 @@
 import { Router } from 'express';
-import { getRepository } from 'typeorm';
+import multer from 'multer';
+// import { getRepository } from 'typeorm';
 
-import User from '../models/User';
+import uploadConfig from '../config/upload';
+
+import authMiddleware from '../middlewares/auth';
+
+// import User from '../models/User';
 import CreateUserService from '../services/CreateUserService';
 
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
-usersRouter.get('/', async (request, response) => {
-  const userRepository = getRepository(User);
+// usersRouter.get('/', async (request, response) => {
+//   const userRepository = getRepository(User);
 
-  const users = await userRepository.find();
+//   const users = await userRepository.find();
 
-  return response.json(users);
-});
+//   return response.json(users);
+// });
 
 usersRouter.post('/', async (request, response) => {
   try {
@@ -31,5 +37,15 @@ usersRouter.post('/', async (request, response) => {
     return response.status(400).json({ error: error.message });
   }
 });
+
+usersRouter.patch(
+  '/avatar',
+  authMiddleware,
+  upload.single('avatar'),
+  async (request, response) => {
+    console.log(request.file);
+    return response.json({ ok: true });
+  },
+);
 
 export default usersRouter;
