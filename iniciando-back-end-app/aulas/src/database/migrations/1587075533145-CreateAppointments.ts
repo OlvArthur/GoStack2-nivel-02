9242feaf585573f1ecb143ff234567cef18446fe
Arthur Oliveira <olvarthur@gmail.com>
2020-04-17 23:33:33 -0300

    Adding relationships

--- iniciando-back-end-app/aulas/src/database/migrations/1587075533145-CreateAppointments.ts
+++ iniciando-back-end-app/aulas/src/database/migrations/1587075533145-CreateAppointments.ts
@@ -9,7 +9,7 @@ export default class CreateAppointments1587075533145
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
@@ -17,12 +17,10 @@ export default class CreateAppointments1587075533145
           {
             name: 'provider',
             type: 'varchar',
-            isNullable: false,
           },
           {
             name: 'date',
             type: 'timestamp with time zone', // Available in postgres only
-            isNullable: false,
           },
           {
             name: 'created_at',
