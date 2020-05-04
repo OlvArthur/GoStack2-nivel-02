c75f96bf6603139aa2bec07e48585a3fae482529
Arthur Oliveira <olvarthur@gmail.com>
2020-05-04 17:52:29 -0300

    Adding cors

--- iniciando-back-end-app/aulas/src/server.ts
+++ iniciando-back-end-app/aulas/src/server.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
 import express, { NextFunction, Response, Request } from 'express';
 import 'express-async-errors';
+import cors from 'cors';
 
 import uploadConfig from './config/upload';
 import routes from './routes';
@@ -10,6 +11,7 @@ import './database';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
